e82222bbe395054f1bec58dab1b32458f9222f6e
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-08-17 22:00:54 +0530

    GET Route

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: true},
+	{ID: "3", Item: "Record Video", Completed: true},
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:8080")
+}
